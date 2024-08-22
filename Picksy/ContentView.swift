cbd221a78fa3efb3ad1b026fa1e8e9aa7c60b678
ebest-weevva <evan@weeevva.com>
2024-08-22 19:01:00 -0230

    Initial Commit

---
+++ Picksy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Picksy
+//
+//  Created by Evan Best on 2024-08-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Picksy/PicksyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicksyApp.swift
+//  Picksy
+//
+//  Created by Evan Best on 2024-08-22.
+//
+
+import SwiftUI
+
+@main
+struct PicksyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
